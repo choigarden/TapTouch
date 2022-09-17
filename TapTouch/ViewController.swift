58c6beaf3acaa3e90da6089feb8042161766c966
jeongwon <choigarden@nate.com>
2022-09-18 01:29:36 +0900

    17-3 탭과 터치 기능 구현하기

--- TapTouch/ViewController.swift
+++ TapTouch/ViewController.swift
@@ -23,10 +23,29 @@ class ViewController: UIViewController {
     }
 
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Began"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
     
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Moved"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
     
-    
-    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Ended"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
 
 }
 
